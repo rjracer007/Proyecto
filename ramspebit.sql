4d11f2ad3011813fe4774a62d8b04c3ada70fd2f
rjracer007 <rjracer007@gmail.com>
2017-03-05 18:44:43 -0500

    Add files via upload

--- ramspebit.sql
+++ ramspebit.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-02-2017 a las 02:02:52
+-- Tiempo de generación: 02-03-2017 a las 13:46:12
 -- Versión del servidor: 10.1.16-MariaDB
 -- Versión de PHP: 7.0.9
 
@@ -24,33 +24,47 @@ DELIMITER $$
 --
 -- Procedimientos
 --
-CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_InsertarEmpleado` (IN `$idEmpleado` INT(11), IN `$nombres` VARCHAR(45), IN `$apellidos` VARCHAR(45), IN `$rh` VARCHAR(45), IN `$celular` INT(11), IN `$fecha_nacimiento` DATE, IN `$correo_electronico` VARCHAR(100), IN `$fecha_ingreso` DATE, IN `$numero_hijos` INT(11), IN `$telefono_fijo` INT(11), IN `$Pension_idPension` INT(11), IN `$Caja_compensacion_idCaja_compensacion` INT(11), IN `$Tipo_contrato_idTipo_contrato` INT(11), IN `$direccion` VARCHAR(45), IN `$barrio` VARCHAR(45), IN `$municipio` VARCHAR(45), IN `$Tipo_documento_idTipo_documento` INT(11), IN `$Genero_idGenero` INT(11), IN `$Cuenta_idCuenta` INT(11), IN `$cesantias` VARCHAR(45), IN `$procesos` VARCHAR(45), IN `$cargo` VARCHAR(45))  NO SQL
-INSERT INTO empleado (idEmpleado, nombres, apellidos, rh, celular, fecha_nacimiento, correo_electronico, fecha_ingreso, numero_hijos, telefono_fijo, Pension_idPension, Caja_compensacion_idCaja_compensacion, Tipo_contrato_idTipo_contrato, direccion, barrio, municipio, Tipo_documento_idTipo_documento, Genero_idGenero, Cuenta_idCuenta, cesantias, procesos, cargo) VALUES ($idEmpleado,$nombres,$apellidos,$rh,$celular,$fecha_nacimiento,
-     $correo_electronico,
-     $fecha_ingreso,
-     $numero_hijos,
-     $telefono_fijo,
-     $Pension_idPension,
-     $Caja_compensacion_idCaja_compensacion,
-     $Tipo_contrato_idTipo_contrato,
-     $direccion,
-     $barrio,
-     $municipio,
-     $Tipo_documento_idTipo_documento,
-     $Genero_idGenero,
-     $Cuenta_idCuenta,
-     $cesantias,
-     $procesos,
-     $cargo)$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_EliminarGenero` (IN `_idGenero` INT)  NO SQL
+DELETE FROM genero WHERE idGenero = _idGenero$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_InsertarEmpleadoEps` (IN `$Empleado_idEmpleado` INT(11), IN `$EPS_idEPS` INT(11))  NO SQL
-insert into empleado_has_eps values ($Empleado_idEmpleado, $EPS_idEPS)$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_EliminarPension` (IN `_idPension` INT)  NO SQL
+DELETE FROM pension WHERE idPension = _idPension$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_InsertarEmpleadoNivelestudio` (IN `$Nivel_estudio_idNivel_estudio` INT(11), IN `$Empleado_idEmpleado` INT(11))  NO SQL
-INSERT INTO nivel_estudio_has_empleado VALUES ($Nivel_estudio_idNivel_estudio, $Empleado_idEmpleado)$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_EliminarTipoContrato` (IN `_idTipo_contrato` INT)  NO SQL
+DELETE FROM tipo_contrato WHERE idTipo_contrato = _idTipo_contrato$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_InsertarEmpleadoProfesion` (IN `$Profesion_idProfesion` INT(11), IN `$Empleado_idEmpleado` INT(11))  NO SQL
-INSERT INTO profesion_has_empleado VALUES ($Profesion_idProfesion, $Empleado_idEmpleado)$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_InsertarEmpleado` (IN `_idEmpleado` INT(11), IN `_nombres` VARCHAR(45), IN `_apellidos` VARCHAR(45), IN `_rh` VARCHAR(45), IN `_celular` INT(11), IN `_fecha_nacimiento` DATE, IN `_correo_electronico` VARCHAR(100), IN `_fecha_ingreso` DATE, IN `_numero_hijos` INT(11), IN `_telefono_fijo` INT(11), IN `_Pension_idPension` INT(11), IN `_Caja_compensacion_idCaja_compensacion` INT(11), IN `_Tipo_contrato_idTipo_contrato` INT(11), IN `_direccion` VARCHAR(45), IN `_barrio` VARCHAR(45), IN `_municipio` VARCHAR(45), IN `_Tipo_documento_idTipo_documento` INT(11), IN `_Genero_idGenero` INT(11), IN `_Cuenta_idCuenta` INT(11), IN `_cesantias` VARCHAR(45), IN `_procesos` VARCHAR(45), IN `_cargo` VARCHAR(45))  NO SQL
+INSERT INTO empleado (idEmpleado, nombres, apellidos, rh, celular, fecha_nacimiento, correo_electronico, fecha_ingreso, numero_hijos, telefono_fijo, Pension_idPension, Caja_compensacion_idCaja_compensacion, Tipo_contrato_idTipo_contrato, direccion, barrio, municipio, Tipo_documento_idTipo_documento, Genero_idGenero, Cuenta_idCuenta, cesantias, procesos, cargo) VALUES (_idEmpleado, _nombres, _apellidos, _rh,_celular,_fecha_nacimiento,_correo_electronico,_fecha_ingreso,_numero_hijos,_telefono_fijo,_Pension_idPension,_Caja_compensacion_idCaja_compensacion,_Tipo_contrato_idTipo_contrato,_direccion,_barrio,_municipio,_Tipo_documento_idTipo_documento,_Genero_idGenero,_Cuenta_idCuenta,_cesantias,_procesos,_cargo)$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_InsertarEmpleadoEps` (IN `_Empleado_idEmpleado` INT(11), IN `_EPS_idEPS` INT(11))  NO SQL
+insert into empleado_has_eps values (_Empleado_idEmpleado, _EPS_idEPS)$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_InsertarEmpleadoNivelestudio` (IN `_Nivel_estudio_idNivel_estudio` INT(11), IN `_Empleado_idEmpleado` INT(11))  NO SQL
+INSERT INTO nivel_estudio_has_empleado VALUES (_Nivel_estudio_idNivel_estudio, _Empleado_idEmpleado)$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_InsertarEmpleadoProfesion` (IN `_Profesion_idProfesion` INT(11), IN `_Empleado_idEmpleado` INT(11))  NO SQL
+INSERT INTO profesion_has_empleado VALUES (_Profesion_idProfesion, _Empleado_idEmpleado)$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_InsertarEps` (IN `_idEPS` INT(11), IN `_nombre` VARCHAR(45), IN `_abreviatura` VARCHAR(45), IN `_fecha_ingreso` DATE, IN `_fecha_retiro` DATE)  NO SQL
+insert into eps (idEPS, nombre, abreviatura, fecha_ingreso, fecha_retiro) VALUES (_idEPS, _nombre, _abreviatura, fecha_ingreso, _fecha_retiro)$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_InsertarGenero` (IN `_idGenero` INT(11), IN `_nombre` VARCHAR(45))  NO SQL
+insert INTO genero (idGenero, nombre)VALUES (_idGenero, _nombre)$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_InsertarNivelEstudio` (IN `_idNivel_estudio` INT(11), IN `_descripcion` VARCHAR(45))  NO SQL
+insert INTO nivel_estudio (idNivel_estudio, descripcion) VALUES (_idNivel_estudio, _descripcion)$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_InsertarPension` (IN `_idPension` INT(11), IN `_nombre` VARCHAR(45), IN `_abreviatura` VARCHAR(45), IN `_fecha_ingreso` DATE, IN `_fecha_retiro` DATE)  NO SQL
+INSERT INTO pension (idPension, nombre, abreviatura, fecha_ingreso, fecha_retiro) VALUES (_idPension, _nombre, _abreviatura, _fecha_ingreso, _fecha_retiro)$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_InsertarProfesion` (IN `_idProfesion` INT(11), IN `_descripcion` VARCHAR(45))  NO SQL
+INSERT into profesion (idProfesion, descripcion) VALUES (_idProfesion, _descripcion)$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_InsertarTipoContrato` (IN `_idTipo_contrato` INT(11), IN `_descripcion` VARCHAR(45))  NO SQL
+insert INTO tipo_contrato (idTipo_contrato, descripcion) VALUES (_idTipo_contrato, _descripcion)$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_InsertarTipoDocumento` (IN `_idTipo_documento` INT(11), IN `_descripcion` VARCHAR(45))  NO SQL
+insert into tipo_documento (idTipo_documento, descripcion) VALUES (_idTipo_documento, _descripcion)$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_ListarEmpleado` ()  NO SQL
 select idEmpleado,	    
@@ -77,32 +91,47 @@ cesantias,
 procesos,	    
 cargo from empleado order by idEmpleado DESC$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_ModificarEmpleado` (IN `$idEmpleado` INT(11), IN `$nombres` VARCHAR(45), IN `$apellidos` VARCHAR(45), IN `$rh` VARCHAR(45), IN `$celular` INT(11), IN `$fecha_nacimiento` DATE, IN `$correo_electronico` VARCHAR(100), IN `$fecha_ingreso` DATE, IN `$numero_hijos` INT(11), IN `$telefono_fijo` INT(11), IN `$Pension_idPension` INT(11), IN `$Caja_compensacion_idCaja_compensacion` INT(11), IN `$Tipo_contrato_idTipo_contrato` INT(11), IN `$direccion` VARCHAR(45), IN `$barrio` VARCHAR(45), IN `$municipio` VARCHAR(45), IN `$Tipo_documento_idTipo_documento` INT(11), IN `$Genero_idGener` INT(11), IN `$Cuenta_idCuenta` INT(11), IN `$cesantias` VARCHAR(45), IN ` $procesos` VARCHAR(45), IN `$cargo` VARCHAR(20))  NO SQL
-UPDATE empleado set    
-nombres = $nombres,	    
-apellidos = $apellidos,	    
-rh = $rh,	    
-celular = $celular,	    
-fecha_nacimiento = $fecha_nacimiento,	    
-correo_electronico = $correo_electronico,	    
-fecha_ingreso = $fecha_ingreso,	    
-numero_hijos = $numero_hijos,	    
-telefono_fijo = $telefono_fijo,	    	    
-Pension_idPension = $Pension_idPension,	    
-Caja_compensacion_idCaja_compensacion = $Caja_compensacion_idCaja_compensacion,	    
-Tipo_contrato_idTipo_contrato = $Tipo_contrato_idTipo_contrato,	    
-direccion = $direccion,	    
-barrio = $barrio,	    
-municipio = $municipio,	    
-Tipo_documento_idTipo_documento = $Tipo_documento_idTipo_documento,	    
-Genero_idGenero = $Genero_idGenero,	    
-Cuenta_idCuenta= $Cuenta_idCuenta,	    
-cesantias = $cesantias,	    
-procesos = $procesos,	    
-cargo = $cargo WHERE idEmpleado = $idEmpleado$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_ListarEps` ()  NO SQL
+SELECT idEPS, nombre, abreviatura, fecha_ingreso, fecha_retiro FROM eps$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_ModificarEstadoEmpleado` (IN `$idEmpleado` INT(11), IN `$estado` BOOLEAN)  NO SQL
-UPDATE empleado set estado = $estado WHERE idEmpleado = $idEmpleado$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_ListarGenero` ()  NO SQL
+SELECT idGenero, nombre from genero$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_ListarNivelEstudio` ()  NO SQL
+SELECT idNivel_estudio, descripcion from nivel_estudio$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_ListarPension` ()  NO SQL
+SELECT idPension, nombre, abreviatura, fecha_ingreso, fecha_retiro from pension$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_ListarProfesion` ()  NO SQL
+SELECT idProfesion, descripcion from profesion$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_ListarTipoContrato` ()  NO SQL
+SELECT idTipo_contrato, descripcion from tipo_contrato$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_ListarTipoDocumento` ()  NO SQL
+SELECT idTipo_documento, descripcion from tipo_documento$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_ModificarEmpleado` (IN `_idEmpleado` INT(11), IN `_nombres` VARCHAR(45), IN `_apellidos` VARCHAR(45), IN `_rh` VARCHAR(45), IN `_celular` INT(11), IN `_fecha_nacimiento` DATE, IN `_correo_electronico` VARCHAR(100), IN `_fecha_ingreso` DATE, IN `_numero_hijos` INT(11), IN `_telefono_fijo` INT(11), IN `_Pension_idPension` INT(11), IN `_Caja_compensacion_idCaja_compensacion` INT(11), IN `_Tipo_contrato_idTipo_contrato` INT(11), IN `_direccion` VARCHAR(45), IN `_barrio` VARCHAR(45), IN `_municipio` VARCHAR(45), IN `_Tipo_documento_idTipo_documento` INT(11), IN `_Genero_idGener` INT(11), IN `_Cuenta_idCuenta` INT(11), IN `_cesantias` VARCHAR(45), IN `_procesos` VARCHAR(45), IN `_cargo` VARCHAR(20))  NO SQL
+UPDATE empleado set nombres = _nombres,	apellidos = _apellidos,	rh = _rh, celular = _celular, fecha_nacimiento = _fecha_nacimiento, correo_electronico = _correo_electronico, fecha_ingreso = _fecha_ingreso, numero_hijos = _numero_hijos,	telefono_fijo = _telefono_fijo,	Pension_idPension = _Pension_idPension,	  Caja_compensacion_idCaja_compensacion = _Caja_compensacion_idCaja_compensacion,	  Tipo_contrato_idTipo_contrato = _Tipo_contrato_idTipo_contrato,	    
+direccion = _direccion,	barrio = _barrio, municipio = _municipio,   
+Tipo_documento_idTipo_documento = _Tipo_documento_idTipo_documento,  Genero_idGenero = _Genero_idGenero, Cuenta_idCuenta= _Cuenta_idCuenta,	 cesantias = _cesantias, procesos = _procesos, cargo = _cargo WHERE idEmpleado = _idEmpleado$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_ModificarEps` (IN `_idEPS` INT(11), IN `_nombre` VARCHAR(45), IN `_abreviatura` VARCHAR(45), IN `_fecha_ingreso` DATE, IN `_fecha_retiro` DATE)  NO SQL
+UPDATE eps set nombre = _nombre, abreviatura = _abreviatura,fecha_ingreso = _fecha_ingreso,fecha_retiro = _fecha_retiro WHERE idEPS = _idEPS$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_ModificarEstadoEmpleado` (IN `_idEmpleado` INT(11), IN `_estado` BOOLEAN)  NO SQL
+UPDATE empleado set estado = _estado WHERE idEmpleado = _idEmpleado$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_ModificarNivelEstudio` (IN `_idNivel_estudio` INT(11), IN `_descripcion` VARCHAR(45))  NO SQL
+UPDATE nivel_estudio SET
+descripcion = _descripcion WHERE idNivel_estudio = _idNivel_estudio$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_ModificarProfesion` (IN `_idProfesion` INT(11), IN `_descripcion` VARCHAR(45))  NO SQL
+UPDATE profesion set descripcion = _descripcion WHERE idProfesion = _idProfesion$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `SP_TipoDocumento` (IN `_idTipo_documento` INT)  NO SQL
+DELETE FROM tipo_documento WHERE idTipo_documento = _idTipo_documento$$
 
 DELIMITER ;
 
@@ -154,12 +183,19 @@ CREATE TABLE `cliente` (
 CREATE TABLE `cuenta` (
   `idCuenta` int(11) NOT NULL,
   `nombre_usuario` varchar(45) NOT NULL,
-  `contraseña` varchar(45) NOT NULL,
+  `contraseña` varchar(100) NOT NULL,
   `correo_electronico` varchar(100) NOT NULL,
   `imagen` varchar(100) DEFAULT NULL,
   `estado` tinyint(1) NOT NULL DEFAULT '1',
   `Rol_idrol` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `cuenta`
+--
+
+INSERT INTO `cuenta` (`idCuenta`, `nombre_usuario`, `contraseña`, `correo_electronico`, `imagen`, `estado`, `Rol_idrol`) VALUES
+(1, 'admin', '2009medellin', 'rjracer007@gmail.com', NULL, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -268,6 +304,13 @@ CREATE TABLE `genero` (
   `idGenero` int(11) NOT NULL,
   `nombre` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `genero`
+--
+
+INSERT INTO `genero` (`idGenero`, `nombre`) VALUES
+(1, 'Masculino');
 
 -- --------------------------------------------------------
 
@@ -451,7 +494,7 @@ CREATE TABLE `producto_has_materia_prima` (
 
 CREATE TABLE `profesion` (
   `idProfesion` int(11) NOT NULL,
-  `Profesioncol` varchar(45) NOT NULL
+  `descripcion` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -477,6 +520,13 @@ CREATE TABLE `rol` (
   `descripcion` varchar(45) NOT NULL,
   `estado` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `rol`
+--
+
+INSERT INTO `rol` (`idrol`, `nombre`, `descripcion`, `estado`) VALUES
+(1, 'administrador', 'encargado de todo', 1);
 
 -- --------------------------------------------------------
 
@@ -832,7 +882,7 @@ ALTER TABLE `unidad_medida`
 -- AUTO_INCREMENT de la tabla `cuenta`
 --
 ALTER TABLE `cuenta`
-  MODIFY `idCuenta` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idCuenta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Restricciones para tablas volcadas
 --
